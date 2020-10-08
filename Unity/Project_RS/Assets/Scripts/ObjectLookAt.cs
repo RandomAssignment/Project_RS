5c8555faa26b1865a4e9afed7c6a999120c95874
To_Ran <41686179+biru100@users.noreply.github.com>
2020-10-08 16:57:58 +0900

    프로토 0.1

---
+++ Unity/Project_RS/Assets/Scripts/ObjectLookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLooAt : MonoBehaviour
+{
+
+    Transform CameraTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraTrans = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(CameraTrans);
+    }
+}
---
+++ Unity/Project_RS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    Transform playertrans;
+    RectTransform stick;
+    SpriteRenderer playerSR;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        stick.position = eventData.position;
+        float value = stick.rect.width / 2 - 30;
+        if (stick.localPosition.magnitude > value)
+        {
+            stick.localPosition = stick.localPosition.normalized * value;
+        }
+        playerSR.flipX = stick.localPosition.x < 0;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        stick.localPosition = Vector3.zero;
+    }
+
+    void Start()
+    {
+        playertrans = GameObject.FindGameObjectWithTag("Player").transform;
+        playerSR = playertrans.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
+        stick = transform.GetChild(0).GetComponent<RectTransform>();
+    }
+
+    void FixedUpdate()
+    {
+        playertrans.Translate(new Vector3(stick.localPosition.normalized.x,0,stick.localPosition.normalized.y) * Time.deltaTime * 10);
+    }
+
+}
